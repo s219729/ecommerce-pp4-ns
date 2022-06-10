5c8ffa931f9f900a828c2882a53fc5e5857261c2
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2022-06-10 20:19:49 +0200

    final for this year !! hjurray

--- src/main/java/pl/jkanclerz/sales/SalesController.java
+++ src/main/java/pl/jkanclerz/sales/SalesController.java
@@ -1,18 +1,22 @@
 package pl.jkanclerz.sales;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.jkanclerz.sales.offerting.Offer;
+import pl.jkanclerz.sales.payment.PaymentDetails;
+
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
 
 @RestController
 public class SalesController {
-    //@ToDo implement session id
-    public static final String CUSTOMER_ID = "Kuba";
 
+    public static final String CURRENT_CUSTOMER_ID_SESSION_KEY = "CURRENT_CUSTOMER_ID";
     Sales sales;
+    private HttpSession httpSession;
 
-    public SalesController(Sales sales) {
+    public SalesController(Sales sales, HttpSession httpSession) {
         this.sales = sales;
+        this.httpSession = httpSession;
     }
 
     @GetMapping("/api/sales/current-offer")
@@ -20,7 +24,24 @@ public class SalesController {
         return sales.getCurrentOffer(getCurrentClientId());
     }
 
+    @PostMapping("/api/sales/add-to-cart/{productId}")
+    void addToCart(@PathVariable String productId) {
+        sales.addToCart(getCurrentClientId(), productId);
+    }
+
+    @PostMapping("/api/sales/accept-offer")
+    PaymentDetails acceptOffer(@RequestBody CustomerData customerData) {
+        return sales.acceptOffer(getCurrentClientId(), customerData);
+    }
+
     private String getCurrentClientId() {
-        return CUSTOMER_ID;
+        Object customerId = httpSession.getAttribute(CURRENT_CUSTOMER_ID_SESSION_KEY);
+
+        if (customerId == null) {
+            customerId = UUID.randomUUID().toString();
+            httpSession.setAttribute(CURRENT_CUSTOMER_ID_SESSION_KEY, customerId);
+        }
+
+        return (String) customerId;
     }
 }
